e66be94050695ce9998a2b110cccb43df3be33ac
Nicolas Deslandes <deslandes.nicolas@gmail.com>
2012-04-05 23:49:43 -0400

    a little refactoring

--- calculator.py
+++ calculator.py
@@ -3,15 +3,29 @@ class Calculator(object):
         pass
     
     def add(self, string):
-        if string.startswith('//'):
-            delimiter_spec, string = string.split('\n', 1)
-            delimiter = delimiter_spec[2:]
-            string = string.replace(delimiter, ',')
-        string = string.replace('\n', ',')
+        string = self._normalize_delimiters(string)
         if string:
-            numbers = map(int,string.split(','))
-            if any(number<0 for number in numbers):
-                raise ValueError
-            return sum(numbers)
+            return self._add_number_in_string(string)
         else:
             return 0
+
+    def _validate_numbers(self, numbers):
+        if any(number<0 for number in numbers):
+            raise ValueError
+            
+    def _normalize_delimiters(self, string):
+        if string.startswith('//'):
+            string = self._normalize_special_delimiters(string)
+        string = string.replace('\n', ',')
+        return string
+        
+    def _normalize_special_delimiters(self, string):
+        delimiter_spec, string = string.split('\n', 1)
+        delimiter = delimiter_spec[2:]
+        string = string.replace(delimiter, ',')
+        return string
+        
+    def _add_number_in_string(self, string):
+        numbers = map(int,string.split(','))
+        self._validate_numbers(numbers)
+        return sum(numbers)
